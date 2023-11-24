9b5d3870b953d659212ee81867ef8e36ff52c203
54m <30588003+54m@users.noreply.github.com>
2023-11-25 07:52:03 +0900

    Add ParallelMap functionality to map.go
    Added a new function, ParallelMap, to map.go to allow for parallel manipulation of a slice that transforms it to a different type. This was implemented to enhance performance by concurrently executing iterations, while maintaining the order of results. It also handles any possible errors which could occur during the execution.

--- map.go
+++ map.go
@@ -1,5 +1,11 @@
 package superlo
 
+import (
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
+
 // Map - returns a new collection of mapped values and error
 func Map[T any, R any](collection []T, iteratee func(item T, index int) (R, error)) ([]R, error) {
 	result := make([]R, len(collection))
@@ -10,6 +16,40 @@ func Map[T any, R any](collection []T, iteratee func(item T, index int) (R, erro
 			return nil, err
 		}
 		result[i] = returnValue
+	}
+
+	return result, nil
+}
+
+// ParallelMap manipulates a slice and transforms it to a slice of another type.
+// `iteratee` is call in parallel. Result keep the same order.
+func ParallelMap[T any, R any](collection []T, iteratee func(item T, index int) (R, error)) ([]R, error) {
+	result := make([]R, len(collection))
+
+	var (
+		eg errgroup.Group
+		mu sync.Mutex
+	)
+
+	for i, item := range collection {
+		func(_item T, _i int) {
+			eg.Go(func() error {
+				returnValue, err := iteratee(_item, _i)
+				if err != nil {
+					return err
+				}
+
+				mu.Lock()
+				result[_i] = returnValue
+				mu.Unlock()
+
+				return nil
+			})
+		}(item, i)
+	}
+
+	if err := eg.Wait(); err != nil {
+		return nil, err
 	}
 
 	return result, nil
